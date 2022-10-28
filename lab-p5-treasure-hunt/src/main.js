afd254093c6b54f8358f5d11b75eaa389beaffa5
Marco Lölkes <marcoloelkes@gmx.de>
2022-10-28 15:45:22 +0200

    Add solution for lab-p5-treasure-hunt

---
+++ lab-p5-treasure-hunt/src/main.js
@@ -0,0 +1,34 @@
+const game = new Game()
+
+function preload() {
+    game.preload()
+} 
+
+function setup() {
+    let canvas = createCanvas(WIDTH, HEIGHT)
+    canvas.parent("canvas")
+    background("red")
+}
+
+function draw() {
+    game.draw()
+}
+
+function keyPressed() {
+
+    if (keyCode === 39) {
+		game.player.moveRight()
+	}
+
+	if (keyCode === 37) {
+		game.player.moveLeft()
+	}
+
+	if (keyCode === 38) {
+		game.player.moveUp()
+	}
+
+	if (keyCode === 40) {
+		game.player.moveDown()
+	}
+}
